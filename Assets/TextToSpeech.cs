eda5d6cd9d2c9c6d1941715d55502bd5af25d9c7
Yasith Hennayake <yasith.ghosty@gmail.com>
2024-02-24 08:50:20 +0530

    finalizing text to speech

--- Assets/TextToSpeech.cs
+++ Assets/TextToSpeech.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5eac1200a149d272f81ba0f8e76585186c5f4d691837b05e3ebb900abc76e71d
-size 4506
+oid sha256:84fc7d56bd354ca0c11ff1eccb563d756740037e659dcd087847e8c94629bca4
+size 4467
