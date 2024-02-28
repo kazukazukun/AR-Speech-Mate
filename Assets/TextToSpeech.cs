658aedec09fbbc261a66380ccb8a153310f5f382
Dexa Milan <explorer2852@gmail.com>
2024-02-28 16:01:39 +0530

    fixing errors in text to speech

--- Assets/TextToSpeech.cs
+++ Assets/TextToSpeech.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84fc7d56bd354ca0c11ff1eccb563d756740037e659dcd087847e8c94629bca4
-size 4467
+oid sha256:34f5896a9a389bd0436d3955a024f3e2885608dc8141801dff3a32d7385f77fc
+size 4337
